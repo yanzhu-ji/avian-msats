d03dba752022821724831f72cb0b0410fa38e3e2
qu <qu@localhost.localdomain>
2020-04-17 16:13:25 +0800

    update

--- master_script.sh
+++ master_script.sh
@@ -178,7 +178,86 @@ for file in `ls ../trf/*.sorted.als5.c18.mdat`; do
     done < ../../all_motif.list
 done
 
+mkdir 3_blast-galGal
+cd 3_blast-galGal
+while read motif; do
+    mkdir $motif
+done < ../../all_motif.list
 
-cd trf
+while read motif; do
+    echo '#!/bin/bash' > ${motif}_3-blast-galGal.sh
+    echo "cd $motif" >> ${motif}_3-blast-galGal.sh
+    for file in `ls ../2_self-blast/${motif}/*_reduced-100.fasta`; do
+        filename=${file##*/}
+        species=${filename%_*-only_cat-flanks_reduced-100.fasta}
+        echo "ln -s ../../2_self-blast/${motif}/$filename ." >> ${motif}_3-blast-galGal.sh
+        echo "makeblastdb -dbtype nucl -in $filename " >> ${motif}_3-blast-galGal.sh
+        echo "blastn -task blastn \
+                  -query $YJI_SPACE/bgi/galGal-${motif}_loci-20.fasta \
+                  -db $filename \
+                  -word_size 4 -reward 1 -penalty -1 \
+                  -qcov_hsp_perc 100 \
+                  -outfmt 6 \
+                  -out galGal_${species}_${motif}_blastn.fmt6 " >> ${motif}_3-blast-galGal.sh
+        echo "cut -f1  galGal_${species}_${motif}_blastn.fmt6| uniq -u > galGal_${species}_${motif}_blastn_unique-query.list" >> ${motif}_3-blast-galGal.sh 
+        echo "grep -f  galGal_${species}_${motif}_blastn_unique-query.list \
+                 galGal_${species}_${motif}_blastn.fmt6 \
+                > galGal_${species}_${motif}_blastn_unique-query.fmt6" >> ${motif}_3-blast-galGal.sh
+        echo 'fmt6_num=`wc -l '  "galGal_${species}_${motif}_blastn.fmt6 | cut -f1 -d" '" "`' >>  ${motif}_3-blast-galGal.sh
+        echo 'uniqQuery_num=`wc -l ' " galGal_${species}_${motif}_blastn_unique-query.fmt6 | cut -f1 -d" '" "`'  >>  ${motif}_3-blast-galGal.sh
+        echo 'if [[ $fmt6_num -ne $uniqQuery_num ]]; then' >> ${motif}_3-blast-galGal.sh 
+        echo "    echo $species $motif has redundant queries of galGal " >>  ${motif}_3-blast-galGal.sh
+        echo "else" >>  ${motif}_3-blast-galGal.sh
+        echo "    rm galGal_${species}_${motif}_blastn.fmt6 " >> ${motif}_3-blast-galGal.sh
+        echo "fi" >> ${motif}_3-blast-galGal.sh
+        echo "rm $filename " >> ${motif}_3-blast-galGal.sh
+        echo "rm ${filename}.* " >> ${motif}_3-blast-galGal.sh 
+        echo "rm galGal_${species}_${motif}_blastn_unique-query.list " >> ${motif}_3-blast-galGal.sh
+        echo "echo ${species} ${motif} blast galGal done" >> ${motif}_3-blast-galGal.sh
+     done 
+     echo "cd .." >> ${motif}_3-blast-galGal.sh
+done < ../../all_motif.list
+
+mkdir 4_genotype
+cd 4_genotype
+while read motif; do
+    mkdir $motif
+done < ../../all_motif.list
 
 
+#loop through each motif, each galGal-locus and each species.
+while read motif; do
+    cd $motif
+    while read galGal_locus; do
+        locusID=`grep $galGal_locus ../../../all_loci_info.txt | cut -f2 `
+        for file in `ls ../../3_blast-galGal/${motif}/*_unique-query.fmt6`; do
+            filename=${file##*/}
+            species=`cut -f2,3 -d"_" <<< $filename`
+            if [ "$(grep -c $galGal_locus $file)" -ne 0 ]; then
+                species_locus=($(grep $galGal_locus $file | cut -f2))
+                cluster_id=`awk -v pattern="$species_locus" '$2~ pattern' ../../2_self-blast/${motif}/${species}_${motif}-only_cat-flanks_silix.fnodes | cut -f1`
+                if [ ! -d "$species_${motif}-only_silix_clusters" ]; then
+                    tar -xf ../../2_self-blast/${motif}/${species}_${motif}-only_silix_clusters.tar
+                fi
+                cd ${species}_${motif}-only_silix_clusters
+                grep ">" ${species}_${motif}-only_cat-flanks_filtered-N_${cluster_id}.fasta |\
+                    sed 's/>//' | cut -f1,2,5 -d"_" | awk -F"_" '{print $1 "\t" $2-1 "\t" $3}' \
+                    > ${species}_${motif}-only_cat-flanks_filtered-N_${cluster_id}.bed
+                cut -f1 ${species}_${motif}-only_cat-flanks_filtered-N_${cluster_id}.bed \
+                    > ${species}_${motif}-only_${cluster_id}_prefa.list
+                faSomeRecords ../../../1_prepare-flank-15bp/${motif}/${species}_${motif}-only.fasta \
+                    ${species}_${motif}-only_${cluster_id}_prefa.list \
+                    ${species}_${motif}-only_${cluster_id}.fasta
+                clip_bed.pl \
+                    -i ${species}_${motif}-only_cat-flanks_filtered-N_${cluster_id}.bed \
+                    -s ${species}_${motif}-only_${cluster_id}.fasta \
+                    > ../${species}_${locusID}.fasta
+                cd ..
+                sed -i "s/>/>${species}_${locusID}_/" ${species}_$locusID.fasta
+                echo $species $locusID of $motif extract sequence done
+            fi
+        done
+    done < ../../../galGal-${motif}_loci-20.list
+    rm -r *_${motif}-only_silix_clusters
+    cd ..
+done < ../../all_motif_2_self-blast-done.list
